51439346ea31bb5846fb11c5ca9473fa961ed76d
Tri Putra Satriawan <75243141+putrasatriawan@users.noreply.github.com>
2024-10-27 04:23:07 +0700

    Update run_owncloud.bash

--- run_owncloud.bash
+++ run_owncloud.bash
@@ -1,28 +1,76 @@
 #!/bin/bash
 
-# Meminta input URL baru dari pengguna
-read -p "Masukkan URL baru untuk OwnCloud (misalnya: cloud.example.com): " new_url
-
-# Backup file config.php sebelum mengedit
-sudo cp /var/www/owncloud/config/config.php /var/www/owncloud/config/config.php.bak
-
-# Menambahkan domain baru ke trusted_domains secara manual
-sudo php -r "
-\$config_file = '/var/www/owncloud/config/config.php';
-\$config = include \$config_file;
-if (!in_array('$new_url', \$config['trusted_domains'])) {
-    \$config['trusted_domains'][] = '$new_url';
-    file_put_contents(\$config_file, '<?php return ' . var_export(\$config, true) . ';');
-    echo 'Trusted domain berhasil ditambahkan: $new_url\n';
-} else {
-    echo 'Domain sudah ada di trusted_domains.\n';
-}
+# Fungsi untuk menampilkan logo dan pesan
+showMe() {
+  echo "@@@@@@@@@@@@@@@@@@@@@@B?!JJ55#@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@G!^~J!?#@@@@@@@@@@@@@@@@@@@
+@@@@@@@&#BBBBBB#&@@@5!7?777J@@@#BBB#BB##&@@@@@@@
+@@@@&#BGGGBBPPPGG&@#7?!77!77#@GBPPPBBBGGBB&@@@@@
+@&&#BGGGGGBBPPPBB&@YJ??J?J??G@BBGPPBBBGGGGB##&@@
+@@#BBGGGGGGBGGG#B&#GGGGGGGGGG&##GGGBGGGGGGBB#@@@
+@##BGGGGGGGBBBB#&B55PPPPPP5G&#BBBBBGGGGGGGB##@@@
+@@&BGGGGGGGBBBBBPYPB@@@@@#PYPBBBBBGGGGGGGGB#@@@
+@@#BBGGGGGGGGG#BY5PPB@@@@@#PG5YB#BPGGGGGGGBB#@@@
+@@&&BBGGGGGGGGGGYGPPB@&P#@#PPG55GGPGGGGGGGB##@@@
+@@@&BBGGGBGBGBGBYGPPB@@#&@#PPG5PBBGBGGGGGGB&@@@@
+@@@&#BBGGBGBBBB@YPPPB@@&@@#PPGY&#BBBBBGGGG##@@@@
+@@@@#BBBBBBB##@@GYGPB@&P#@#PG5P@@##BBBBBBBB@@@@@
+@@@&BBBBBB#&@&##&G5PB@@@@@#P5P&&##@&#B#BBBB&@@@@
+@@@#BB#B##@@@#5PB&B5P&@@@&GYG&#PPG@@@&#B#B#B@@@@
+@@#BB###@@@@@@BPPPGBG5PGP5PBBPPPG@@@@@@&B#B##@@@
+@&####&@@@@&BPY5PP5PPBGPGGPP5PP5J5B&@@@@&#####@@
+&###@@@@@@@&GJYJY555PYB?G55PP55JYY5#@@@@@@@&##&@
+#&@@@@@@@@@@@@BGYJ5J5P5?YP5YYYJGB&@@@@@@@@@@@@#&
+@@@@@@@@@@@@@@@@&@BG#BGBGG#GG&&@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 "
+}
 
-# Memperbarui konfigurasi Apache dengan URL baru
+# Menampilkan logo dan pesan selamat datang
+showMe
+echo "Selamat datang di Instalasi OwnCloud!"
+sleep 2s
+
+# Meminta URL kustom dari pengguna
+read -p "Masukkan URL kustom untuk OwnCloud (misalnya: cloud.example.com): " custom_url
+
+# Memperbarui dan menginstal paket yang diperlukan
+sudo apt update && sudo apt upgrade -y
+sudo apt install apache2 mariadb-server php7.4 libapache2-mod-php7.4 \
+php7.4-{mysql,intl,curl,json,gd,xml,mbstring,zip} curl gnupg2 -y
+
+# Menambahkan repository OwnCloud dan menginstalnya
+echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/server:/10.9.1/Ubuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/owncloud.list
+curl -fsSL https://download.opensuse.org/repositories/isv:ownCloud:server:/10/Ubuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/owncloud.gpg > /dev/null
+sudo apt update
+sudo apt install owncloud-complete-files -y
+
+# Membuat direktori OwnCloud dan memberikan izin
+sudo mkdir -p /var/www/owncloud
+sudo chown -R www-data:www-data /var/www/owncloud
+
+# Konfigurasi database MySQL
+sudo mysql --user=root << EOF
+CREATE DATABASE IF NOT EXISTS ownclouddb;
+GRANT ALL PRIVILEGES ON ownclouddb.* TO 'ownclouduser'@'localhost' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+EOF
+
+# Menjalankan instalasi OwnCloud melalui CLI
+sudo -u www-data php /var/www/owncloud/occ maintenance:install \
+   --database "mysql" \
+   --database-name "ownclouddb" \
+   --database-user "ownclouduser" \
+   --database-pass "password" \
+   --admin-user "admin" \
+   --admin-pass "admin"
+
+# Membuat konfigurasi Apache dengan URL kustom
 sudo tee /etc/apache2/sites-available/owncloud.conf > /dev/null << EOL
 <VirtualHost *:80>
-  ServerName $new_url
+  ServerName $custom_url
   DocumentRoot /var/www/owncloud
 
   <Directory /var/www/owncloud>
@@ -47,9 +95,7 @@ EOL
 sudo a2dissite 000-default.conf
 sudo a2ensite owncloud.conf
 sudo a2enmod rewrite
-
-# Restart Apache untuk menerapkan perubahan
 sudo systemctl restart apache2
 
 # Pesan akhir
-echo "OwnCloud sekarang berjalan di URL: http://$new_url"
+echo "Instalasi OwnCloud selesai! OwnCloud sekarang dapat diakses melalui: http://$custom_url"
